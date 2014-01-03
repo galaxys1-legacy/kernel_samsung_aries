ba1acea03d9ab2e3a5d2915ac2ab500d243bd776
Laura Abbott <lauraa@codeaurora.org>
2014-01-03 10:47:00 -0800

    fs: fuse: Add replacment for CMA pages into the LRU cache
    CMA pages are currently replaced in the FUSE file system since
    FUSE may hold on to CMA pages for a long time, preventing migration.
    The replacement page is added to the file cache but not the LRU
    cache. This may prevent the page from being properly aged and dropped,
    creating poor performance under tight memory condition. Fix this by
    adding the new page to the LRU cache after creation.
    
    Change-Id: Ib349abf1024d48386b835335f3fbacae040b6241
    CRs-Fixed: 586855
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -14,6 +14,10 @@
 #include <linux/sched.h>
 #include <linux/module.h>
 #include <linux/compat.h>
+#ifdef CONFIG_CMA
+/* Needed for lru_cache_add_lru() */
+#include <linux/swap.h>
+#endif
 
 static const struct file_operations fuse_direct_io_file_operations;
 
@@ -676,6 +680,8 @@ static int fuse_readpages_fill(void *_data, struct page *page)
 		 * owner of it
 		 */
 		put_page(newpage);
+
+		lru_cache_add_lru(newpage, LRU_ACTIVE_FILE);
 
 		/* finally release the old page and swap pointers */
 		unlock_page(oldpage);
