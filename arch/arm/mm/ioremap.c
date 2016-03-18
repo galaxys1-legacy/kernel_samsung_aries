11847598148c8e84a2b2748f56524b3548208f8f
Luden <luden@ghostmail.com>
2016-03-18 12:12:02 +0100

    ARM: ioremap: allow CMA memory to be mapped for our purposes
    This conditionally reverts https://github.com/omnirom/android_kernel_samsung_aries/commit/aa68b9ca40c316b79402f080fde998ee2465aca2
    
    xc-racer99: Fix for when CMA is not selected

--- arch/arm/mm/ioremap.c
+++ arch/arm/mm/ioremap.c
@@ -194,6 +194,7 @@ void __iomem * __arm_ioremap_pfn_caller(unsigned long pfn,
 	int err;
 	unsigned long addr;
  	struct vm_struct * area;
+	bool is_pfn_valid;
 
 	/*
 	 * High mappings must be supersection aligned
@@ -203,10 +204,22 @@ void __iomem * __arm_ioremap_pfn_caller(unsigned long pfn,
 
 	/*
 	 * Don't allow RAM to be mapped - this causes problems with ARMv6+
-
-	if (WARN_ON(pfn_valid(pfn)))
+	 */
+#ifdef CONFIG_CMA
+	/* Skipping the check for CMA areas is a hack that allows CMA memory to be mapped for
+	 * our purposes. Given the memory is not simultaneously used for anything else,
+	 * it should be fine. However, the proper fix would be to remove these pages from
+	 * 'memory' regions, flush the TLB, etc - see DMA allocation parts of the original
+	 * CMA implementation. */
+	is_pfn_valid = pfn_valid(pfn);
+	if (is_pfn_valid && get_pageblock_migratetype(pfn_to_page(pfn)) != MIGRATE_CMA) {
+		WARN_ON(is_pfn_valid);
 		return NULL;
-	*/
+	}
+#else
+  	if (WARN_ON(pfn_valid(pfn)))
+  		return NULL;
+#endif
 
 	type = get_mem_type(mtype);
 	if (!type)
