d0a5faf2253b5dcebcdc722a170f21d8b94a3312
Coldwindofnowhere <coldwindofnowhere@gmail.com>
2016-06-21 07:04:47 +0200

    Revert "Merge "aries|p1: introduce bigmem support" into android-5.1"
    This reverts commit a9e4285958a25c7d733a7ef3d6ed62cecbacbdd6, reversing
    changes made to 9b2263a1e4f869a8a4985262de77bb4b579866f6.

--- arch/arm/mach-s5pv210/mach-aries.c
+++ arch/arm/mach-s5pv210/mach-aries.c
@@ -106,9 +106,6 @@
 #undef pr_debug
 #define pr_debug pr_info
 
-bool bigmem;
-EXPORT_SYMBOL(bigmem);
-
 struct class *sec_class;
 EXPORT_SYMBOL(sec_class);
 
@@ -149,16 +146,9 @@ static int aries_notifier_call(struct notifier_block *this,
 
 	if ((code == SYS_RESTART) && _cmd) {
 		if (!strcmp((char *)_cmd, "recovery"))
-			if (bigmem)
-				mode = 9;
-			else
-				mode = 2; // It's not REBOOT_MODE_RECOVERY, blame Samsung
-		else {
-			if (bigmem)
-				mode = 7;
-			else
-				mode = REBOOT_MODE_NONE;
-		}
+			mode = 2; // It's not REBOOT_MODE_RECOVERY, blame Samsung
+		else
+			mode = REBOOT_MODE_NONE;
 	}
 	__raw_writel(mode, S5P_INFORM6);
 
@@ -320,11 +310,9 @@ static struct s3cfb_lcd s6e63m0 = {
 };
 
 #define  S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC0 (12288 * SZ_1K)
-#define  S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC0_BM (5000 * SZ_1K)
 // Disabled to save memory (we can't find where it's used)
 //#define  S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC1 (9900 * SZ_1K)
 #define  S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC2 (12288 * SZ_1K)
-#define  S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC2_BM (5000 * SZ_1K)
 #define  S5PV210_VIDEO_SAMSUNG_MEMSIZE_MFC0 (14336 * SZ_1K)
 #define  S5PV210_VIDEO_SAMSUNG_MEMSIZE_MFC1 (21504 * SZ_1K)
 #define  S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMD (S5PV210_LCD_WIDTH * \
@@ -5322,19 +5310,6 @@ static struct platform_device *aries_devices[] __initdata = {
 
 };
 
-static void check_bigmem(void) {
-	int bootmode = __raw_readl(S5P_INFORM6);
-	if ((bootmode == 7) || (bootmode == 9)) {
-		bigmem = true;
-		aries_media_devs[2].memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC0_BM;
-		aries_media_devs[3].memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC2_BM;
-	} else {
-		bigmem = false;
-		aries_media_devs[2].memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC0;
-		aries_media_devs[3].memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC2;
-	}
-}
-
 static void __init aries_map_io(void)
 {
 	s5p_init_io(NULL, 0, S5P_VA_CHIPID);
@@ -5344,7 +5319,6 @@ static void __init aries_map_io(void)
 	#ifndef CONFIG_S5P_HIGH_RES_TIMERS
 		s5p_set_timer_source(S5P_PWM3, S5P_PWM4);
 	#endif
-	check_bigmem();
 	s5p_reserve_bootmem(aries_media_devs,
 		ARRAY_SIZE(aries_media_devs), S5P_RANGE_MFC);
 #ifdef CONFIG_MTD_ONENAND
